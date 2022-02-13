862ff7915c2930ddc99fe3471ea3490682845b29
Jeffery Forbes <jefferyforbes.1@outlook.com>
2022-02-13 13:01:58 +0000

    mongo config

--- src/main/java/com/example/fantasticrestaurantv1/DatabaseConfig.java
+++ src/main/java/com/example/fantasticrestaurantv1/DatabaseConfig.java
@@ -1,4 +1,19 @@
 package com.example.fantasticrestaurantv1;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseConfig {
+    ConnectionString connectionString =
+            new ConnectionString(System.getenv("DATABASE_URL"));
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build();
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("restaurantsDB");
 }
